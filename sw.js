5f3a1e8c966c407f5ce216d21c6fdaef0df29d4a
smartyshakil <smartyshakil@gmail.com>
2026-09-20 16:07:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'empire-catalog-v3.18';
-const IMAGE_CACHE_NAME = 'empire-images-v3.18';
+const CACHE_NAME = 'empire-catalog-v3.19';
+const IMAGE_CACHE_NAME = 'empire-images-v3.19';
 
 const ASSETS_TO_CACHE = [
   './',
